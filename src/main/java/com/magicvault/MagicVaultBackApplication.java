61a1196e05ce46bd160ed200a69cec853b7c9ba6
CristopherGarcia2000 <usr3587@salesianos-lacuesta.net>
2024-05-24 17:42:01 +0100

    first commit

---
+++ src/main/java/com/magicvault/MagicVaultBackApplication.java
@@ -0,0 +1,13 @@
+package com.magicvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicVaultBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicVaultBackApplication.class, args);
+	}
+
+}
